1549cce7165f71d67de2bd0d1bedd48b9a00b41b
Ravi Singh <ravisingh02082001@gmail.com>
2022-03-04 18:02:29 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import './index.css'
 
 ReactDOM.render(
-    <SpeechProvider appId='6d21bc23-08df-429d-a529-d532c0992ab6' language='en-US'>
+    <SpeechProvider appId='Speechly-Key from speechly dashboard' language='en-US'>
         <Provider>
             <App />
         </Provider>
